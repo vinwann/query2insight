364eee57422e0d5ae85ac6bec689d757ee344936
Vinura Wanniarachchi <vinura.sahan@gmail.com>
2024-08-21 00:25:08 +0530

    initial

---
+++ app.py
@@ -0,0 +1,4 @@
+import jaclang as jac
+
+app = jac.jac_import("streamlit_fronend", base_path=".")
+app.main()
---
+++ db_service.py
@@ -0,0 +1,42 @@
+from tinydb import TinyDB
+import os
+
+class TinyDBService:
+    def __init__(self, db_file_name=None):
+        if db_file_name is None:
+            db_file_name = "default_db"
+        
+        self.db_file = os.path.join("db", f"{db_file_name}.json")
+        if not os.path.exists("db"):
+            os.makedirs("db")
+
+        self.db = self.create_db()
+
+    def create_db(self):
+        """Creates and returns a TinyDB instance."""
+        try:
+            db = TinyDB(self.db_file)
+            return db
+        except Exception as e:
+            print(f"Error creating database: {e}")
+            return None
+
+    def insert_data(self, data):
+        if not isinstance(data, dict):
+            print("Invalid data format. Data must be a dictionary.")
+            return False
+        
+        try:
+            self.db.insert(data)
+            return True
+        except Exception as e:
+            print(f"Error inserting data: {e}")
+            return False
+
+    def return_chat_history(self):
+        try:
+            records = self.db.all()
+            return records
+        except Exception as e:
+            print(f"Error retrieving records: {e}")
+            return []
